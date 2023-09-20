8b61aa9ce6fc1b4a3c1e4b3b2096a3b089fedd08
aadityaxD <96954590+aadityaxD@users.noreply.github.com>
2023-09-20 13:18:09 +0530

    Update Challenge 2.1 .py

--- Unit 2/Challenge 2.1 .py
+++ Unit 2/Challenge 2.1 .py
@@ -3,6 +3,7 @@ class BankAccount:
         self.__account_number = account_number
         self.__account_holder_name = account_holder_name
         self.__account_balance = initial_balance
+        self.__minimum_balance = 500.0
 
     def deposit(self, amount):
         if amount > 0:
@@ -12,21 +13,35 @@ class BankAccount:
             print("Invalid deposit amount. Amount must be greater than 0.")
 
     def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance:.2f}")
+        if amount > 0:
+            if self.__account_balance - amount >= self.__minimum_balance:
+                self.__account_balance -= amount
+                print(f"Withdrew ${amount}. New balance: ${self.__account_balance:.2f}")
+            else:
+                print("Insufficient balance. Need a minimum balance of $500 to withdraw.")
         else:
-            print("Invalid withdrawal amount or insufficient balance.")
+            print("Invalid withdrawal amount. Amount must be greater than 0.")
 
     def display_balance(self):
         print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance:.2f}")
 
 account = BankAccount("12345", "John Doe", 1000.0)
 
-deposit_amount = float(input("Enter the amount to deposit: "))
-withdraw_amount = float(input("Enter the amount to withdraw: "))
+while True:
+    print("\nMenu:")
+    print("1. Withdraw cash")
+    print("2. Deposit money")
+    print("3. Exit Atm")
+    choice = input("Enter your choice: ")
 
-account.display_balance()
-account.deposit(deposit_amount)
-account.withdraw(withdraw_amount)
-account.display_balance()
+    if choice == '1':
+        withdraw_amount = float(input("Enter the amount to withdraw: "))
+        account.withdraw(withdraw_amount)
+    elif choice == '2':
+        deposit_amount = float(input("Enter the amount to deposit: "))
+        account.deposit(deposit_amount)
+    elif choice == '3':
+        print("No Free Money.")
+        break
+    else:
+        print("Invalid choice. Please enter a valid option (1, 2, or 3).")
